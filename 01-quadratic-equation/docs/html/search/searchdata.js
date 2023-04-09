21fd0169161b27b751ac7d3b77c9e4b45c34b9ed
ProfessorEger <profesegor@gmail.com>
2023-04-09 20:18:53 +0300

    ku03-04: fixed bug in 01-quadratic-equation/docs/config-docs

--- 01-quadratic-equation/docs/html/search/searchdata.js
+++ 01-quadratic-equation/docs/html/search/searchdata.js
@@ -1,12 +1,33 @@
 var indexSectionsWithContent =
 {
+  0: "ceilnpqrsx",
+  1: "elq",
+  2: "cis",
+  3: "ciprs",
+  4: "cnx",
+  5: "elq",
+  6: "n"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations"
 };
 
